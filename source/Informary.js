1831e8259fbe5f03ecc71bfb32ef883f34b28cf7
Steven Velozo <steven@velozo.com>
2020-11-17 11:39:05 -0800

    Added segregated data management functions for complex form data addresses.

--- source/Informary.js
+++ source/Informary.js
@@ -29,6 +29,89 @@ class Informary
 		this.log = this._Log;
 	}
 
+	getValueAtAddress (pObject, pAddress)
+	{
+		// Make sure pObject is an object
+		if (!typeof(pObject) === 'object') return false;
+		// Make sure pAddress is a string
+		if (!typeof(pAddress) === 'string') return false;
+
+		let tmpSeparatorIndex = pAddress.indexOf('.');
+
+		if (tmpSeparatorIndex === -1)
+		{
+			// Now is the time to return the value in the address
+			return pObject[pAddress];
+		}
+		else
+		{
+			let tmpSubObjectName = pAddress.substring(0, tmpSeparatorIndex);
+			let tmpNewAddress = pAddress.substring(tmpSeparatorIndex+1);
+
+			// If there is an object property already named for the sub object, but it isn't an object
+			// then the system can't set the value in there.  Error and abort!
+			if (pObject.hasOwnProperty(tmpSubObjectName) && typeof(pObject[tmpSubObjectName]) !== 'object')
+			{
+				return false;
+			}
+			else if (pObject.hasOwnProperty(tmpSubObjectName))
+			{
+				// If there is already a subobject pass that to the recursive thingy
+				return this.getValueAtAddress(pObject[tmpSubObjectName], tmpNewAddress);
+			}
+			else
+			{
+				// Create a subobject and then pass that
+				pObject[tmpSubObjectName] = {};
+				return this.getValueAtAddress(pObject[tmpSubObjectName], tmpNewAddress);
+			}
+		}
+	}
+
+	setValueAtAddress (pObject, pAddress, pValue)
+	{
+		// Make sure pObject is an object
+		if (!typeof(pObject) === 'object') return false;
+		// Make sure pAddress is a string
+		if (!typeof(pAddress) === 'string') return false;
+
+		let tmpSeparatorIndex = pAddress.indexOf('.');
+
+		if (tmpSeparatorIndex === -1)
+		{
+			// Now is the time to set the value in the object
+			pObject[pAddress] = pValue;
+			return true;
+		}
+		else
+		{
+			let tmpSubObjectName = pAddress.substring(0, tmpSeparatorIndex);
+			let tmpNewAddress = pAddress.substring(tmpSeparatorIndex+1);
+
+			// If there is an object property already named for the sub object, but it isn't an object
+			// then the system can't set the value in there.  Error and abort!
+			if (pObject.hasOwnProperty(tmpSubObjectName) && typeof(pObject[tmpSubObjectName]) !== 'object')
+			{
+				if (!pObject.hasOwnProperty('__ERROR'))
+					pObject['__ERROR'] = {};
+				// Put it in an error object so data isn't lost
+				pObject['__ERROR'][pAddress] = pValue;
+				return false;
+			}
+			else if (pObject.hasOwnProperty(tmpSubObjectName))
+			{
+				// If there is already a subobject pass that to the recursive thingy
+				return this.setValueAtAddress(pObject[tmpSubObjectName], tmpNewAddress, pValue);
+			}
+			else
+			{
+				// Create a subobject and then pass that
+				pObject[tmpSubObjectName] = {};
+				return this.setValueAtAddress(pObject[tmpSubObjectName], tmpNewAddress, pValue);
+			}
+		}
+	}
+
 	marshalDataToForm (pRecordObject, fCallback, pParentPropertyAddress)
 	{
 		let tmpOperationTime = this.log.getTimeStamp();
@@ -119,50 +202,6 @@ class Informary
 
 		let tmpUnknownValueIndex = 0;
 
-		let setValueAtAddress = (pObject, pAddress, pValue) =>
-		{
-			// Make sure pObject is an object
-			if (!typeof(pObject) === 'object') return false;
-			// Make sure pAddress is a string
-			if (!typeof(pAddress) === 'string') return false;
-
-			let tmpSeparatorIndex = pAddress.indexOf('.');
-
-			if (tmpSeparatorIndex === -1)
-			{
-				// Now is the time to set the value in the object
-				pObject[pAddress] = pValue;
-				return true;
-			}
-			else
-			{
-				let tmpSubObjectName = pAddress.substring(0, tmpSeparatorIndex);
-				let tmpNewAddress = pAddress.substring(tmpSeparatorIndex+1);
-
-				// If there is an object property already named for the sub object, but it isn't an object
-				// then the system can't set the value in there.  Error and abort!
-				if (pObject.hasOwnProperty(tmpSubObjectName) && typeof(pObject[tmpSubObjectName]) !== 'object')
-				{
-					if (!pObject.hasOwnProperty('__ERROR'))
-						pObject['__ERROR'] = {};
-					// Put it in an error object so data isn't lost
-					pObject['__ERROR'][pAddress] = pValue;
-					return false;
-				}
-				else if (pObject.hasOwnProperty(tmpSubObjectName))
-				{
-					// If there is already a subobject pass that to the recursive thingy
-					return setValueAtAddress(pObject[tmpSubObjectName], tmpNewAddress, pValue);
-				}
-				else
-				{
-					// Create a subobject and then pass that
-					pObject[tmpSubObjectName] = {};
-					return setValueAtAddress(pObject[tmpSubObjectName], tmpNewAddress, pValue);
-				}
-			}
-		};
-
 		this._Dependencies.jquery.each(tmpFormValueElements,
 			(pRecordIndex, pRecordAddress) =>
 			{
@@ -185,7 +224,7 @@ class Informary
 					tmpFormValueAddress = '__ERROR.UnsetDatum.'+tmpUnknownValueIndex;
 					tmpUnknownValueIndex++;
 				}
-				setValueAtAddress(pRecordObject, tmpFormValueAddress, tmpFormValue);
+				this.setValueAtAddress(pRecordObject, tmpFormValueAddress, tmpFormValue);
 			});
 
 		return fCallback();
